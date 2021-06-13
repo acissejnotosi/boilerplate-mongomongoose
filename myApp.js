3a71982c36b9b8288c3f1dcda3a075aaa72df0ea
Jessica Isoton <jessicasampaio195@gmail.com>
2021-06-13 20:08:43 +0200

    added favoritefoods in Schema

--- myApp.js
+++ myApp.js
@@ -6,12 +6,13 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 const personSchema = new Schema({
   name: { type: String, require: true },
-  age: Number
+  age: Number,
+  favoriteFoods: [String]
 });
 
 let Person = mongoose.model('Person', personSchema);
 
-console.log(Person);
+//console.log(Person.schema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
