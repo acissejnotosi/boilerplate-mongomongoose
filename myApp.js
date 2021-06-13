f1011a7de4477befdbf2b5046595f5e5672b949f
Jessica Isoton <jessicasampaio195@gmail.com>
2021-06-13 20:04:41 +0200

    added personSchema, created Person model

--- myApp.js
+++ myApp.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 var mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = new Schema({
+  name: { type: String, require: true },
+  age: Number
+});
+
+let Person = mongoose.model('Person', personSchema);
+
+console.log(Person);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
