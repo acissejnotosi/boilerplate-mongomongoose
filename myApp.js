5ac7cc4e13f92fd4d8fe5db91b7a07272a8ee698
Jessica Isoton <jessicasampaio195@gmail.com>
2021-06-14 11:09:53 +0200

    remove many

--- myApp.js
+++ myApp.js
@@ -92,9 +92,9 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-  Person.findOneAndRemove({ name: nameToRemove }, {}, (err, deletedPerson) => {
+  Person.remove({ name: nameToRemove }, (err, response) => {
     if (err) return console.log(err);
-    done(null, deletedPerson);
+    done(null, response);
   });
 };
 
